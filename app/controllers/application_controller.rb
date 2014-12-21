18e401f287b5178e60f293be861440ad5ebcbfc1
Michael Barrington Stone <hi@michaelbstone.net>
2014-12-20 19:33:24 -0600

    Application Controller changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::Base
   end
 
   def global_request_logging
-  	http_request_header_keys = request.headers.keys.select{|header_name| header_name.mtch("^HTTP.*")}
+  	http_request_header_keys = request.headers.keys.select{|header_name| header_name.match("^HTTP.*")}
   	http_request_headers = request.headers.select{|header_name, header_value| http_request_header_keys.index(header_name)}
   	logger.info "Received #{request.method.inspect} to #{request.url.inspect} from #{request.remote_ip.inspect}. Processing with headers #{http_request_headers.inspect} and params #{params.inspect}"
   	begin
